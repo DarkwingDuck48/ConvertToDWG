5289b8db5225eb62648625e2b516ade317d749db
Max Britvin <maksbritvin@gmail.com>
2016-08-22 11:59:32 +0300

    Delete testing.py

--- testing.py
+++
@@ -1,9 +0,0 @@
-import ezdxf
-import os
-
-
-path_to_file = os.path.normpath("D:\Проекты\Мой проект\DXFtest\Соловьевка.dxf")
-dxf = ezdxf.readfile(path_to_file)
-msp = dxf.modelspace()
-for e in dxf.entities:
-    print("DXF type: %s" % e.dxftype())
